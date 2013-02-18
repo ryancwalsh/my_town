8b9b916f6a501342549bf736b32d034590d7369f
Ryan C Walsh <my_town@fakemytown.com>
2013-02-17 19:09:19 -0800

    reorganizing functions

--- js/main.js
+++ js/main.js
@@ -23,10 +23,7 @@ function convertSpotToForm(spot, form){
         });
         var commaList = tags.join(', ');
         form.find('input[name="tags"]').importTags(commaList);
-    },
-    function(error) {
-        console.log("Error: " + error.code + " " + error.message);
-    });
+    }, logErr);
     form.find('input[name="id"]').val(spot['id']);
     var textFields = ['name', 'notes', 'website', 'yelp', 'menu', 'icon'];
     $.each(textFields, function(k, v){
@@ -43,6 +40,16 @@ function convertSpotToForm(spot, form){
             field.removeAttr(checked);
         }
     });
+}
+
+function getTag(tagValue, tags){
+    var matchingTag = false;
+    $.each(tags, function(k, v){
+        if(v.get('value').toUpperCase() == tagValue.toUpperCase()){
+            matchingTag = v;
+        }
+    });
+    return matchingTag;
 }
 
 $(document).ready(function(){
@@ -68,71 +75,9 @@ $(document).ready(function(){
             var field = form.find('input[name="' + v + '"]');        
             var bool = field.is(':checked');
             spot.set(v, bool);
-        });
-        var relation = spot.relation("tags");
+        });        
         var tagValues = form.find('input[name="tags"]').val().split(',');
-        //Remove Tags from Spot if no longer in input field:
-        relation.query().find().then(function(results){
-            $.each(results, function(k, v){
-                console.log(v.get('value'));
-                if ($.inArray(v.get('value'), tagValues) === -1){
-                    console.log('remove');
-                    relation.remove(v);
-                //TODO: if this Tag is no longer related to any Spots, delete the Tag completely.
-                }
-            });
-        },
-        function(error) {
-            console.log("Error: " + error.code + " " + error.message);
-        }).then(function(){
-            var tags = [];
-            var query = new Parse.Query(Tag);
-            query.equalTo("user", user);
-            query.find().then(function(results) {
-                $.each(results, function(k, v){
-                    tags.push(v);
-                });
-                console.log(tags);
-            }, standardError).then(function(){
-                //Add Tags to Spot if Tag exists in input field but not Spot:
-                $.each(tagValues, function(k, v){
-                    console.log(tags);
-                    var tag = getTag(v, tags);
-                    if(tag) {
-                        //Add the relation of the Tag to this Spot.                
-                        console.log('Tag already existed, so adding relation.');
-                        relation.add(tag);
-                    } else {
-                        //If this Tag does not yet exist for this user, create it.
-                        tag = new Tag();
-                        tag.set('user', user);
-                        tag.set('ACL', new Parse.ACL(user));
-                        tag.set('value', v);
-                        console.log('Created new Tag.');
-                        tag.save().then(function(tag) {
-                            //Add the relation of the Tag to this Spot.                
-                            console.log('Saved Tag. Adding relation.');
-                            relation.add(tag);
-                        }, standardError);
-                    }                
-                });
-            }, standardError).then(function(){
-                console.log(spot);
-                spot.save().then(function(data) {                    
-                    console.log('Saved Spot.');
-                }, standardError);
-            });
-        });
+        handleTags(spot, tagValues, user);
     });
-    
-    function getTag(tagValue, tags){
-        var matchingTag = false;
-        $.each(tags, function(k, v){
-            if(v.get('value').toUpperCase() == tagValue.toUpperCase()){
-                matchingTag = v;
-            }
-        });
-        return matchingTag;
-    }
-    
+        
 });//end doc ready
--- js/parse.js
+++ js/parse.js
@@ -3,7 +3,7 @@ Parse.$ = jQuery;
 // Initialize Parse with your Parse application javascript keys
 Parse.initialize("yaH11lzPCB3w9ExzINJfcRq34YK1u7yRuUgOPko3", "uNYybtZweTwox5bW1ETykpkNdZhvHaaQCn0BFpTU");
 
-function standardError(data, error) {                
+function logErr(data, error) {                
     console.log("Error: " + error.code + " " + error.message);
     console.log(data);
 }
@@ -13,7 +13,7 @@ var tempUserObj;
 Parse.User.logIn('rcwalsh', 'parse', {}).then(function(user) {
     tempUserObj = user;
     console.log(tempUserObj);
-}, standardError);
+}, logErr);
 //-----------------------------------------------------------------------------------
 
 
@@ -39,7 +39,7 @@ function getSpotsForUser(user, map){//TODO: this isn't the way to handle an asyn
             mySpots[v.id] = v;
         });
     },
-    standardError);
+    logErr);
     return mySpots;
 }
 
@@ -57,12 +57,64 @@ function saveGoogResultToSpot(user, result, map){
         latitude: result.lat, 
         longitude: result.lng
     }));
-    spot.save(null, {}).then(function(spot) {
-        // The object was saved successfully.
+    spot.save().then(function(spot) {
         addSpotToShownListAndMap(spot, map);
         mySpots[spot.id] = spot;
         $('#searchTextField').val('');
     },
-    standardError
+    logErr
     );
+}
+
+function handleTags(spot, tagValues, user){
+    var relation = spot.relation("tags");
+    //Remove Tags from Spot if no longer in input field:
+    relation.query().find().then(function(results){
+        $.each(results, function(k, v){
+            console.log(v.get('value'));
+            if ($.inArray(v.get('value'), tagValues) === -1){
+                console.log('remove');
+                relation.remove(v);
+            //TODO: if this Tag is no longer related to any Spots, delete the Tag completely.
+            }
+        });
+    }, logErr).then(function(){
+        var tags = [];
+        var query = new Parse.Query(Tag);
+        query.equalTo("user", user);
+        query.find().then(function(results) {
+            $.each(results, function(k, v){
+                tags.push(v);
+            });
+            console.log(tags);
+        }, logErr).then(function(){
+            //Add Tags to Spot if Tag exists in input field but not Spot:
+            $.each(tagValues, function(k, v){
+                console.log(tags);
+                var tag = getTag(v, tags);
+                if(tag) {
+                    //Add the relation of the Tag to this Spot.                
+                    console.log('Tag already existed, so adding relation.');
+                    relation.add(tag);
+                } else {
+                    //If this Tag does not yet exist for this user, create it.
+                    tag = new Tag();
+                    tag.set('user', user);
+                    tag.set('ACL', new Parse.ACL(user));
+                    tag.set('value', v);
+                    console.log('Created new Tag.');
+                    tag.save().then(function() {
+                        //Add the relation of the Tag to this Spot.                
+                        console.log('Saved Tag. Adding relation.');
+                        relation.add(tag);
+                    }, logErr);
+                }                
+            });
+        }, logErr).then(function(){
+            console.log(spot);
+            spot.save().then(function(data) {                    
+                console.log('Saved Spot.');
+            }, logErr);
+        });
+    });
 }
